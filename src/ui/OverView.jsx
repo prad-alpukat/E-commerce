c55b0ffcbf2d5f8e8460bb325d3f498acd7b6160
Rifan <rifan@example.com>
2024-09-10 21:59:39 +0700

    membuat pagesbaru

---
+++ src/ui/OverView.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function OverView() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className=" py-10 ">
+      <h1 className="text-4xl font-bold mb-6 text-white ms-6">Overview</h1>
+      <div className="flex overflow-x-auto space-x-2 scrollbar-hide p-4 glass backdrop-brightness-50">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center w-64 flex-shrink-0 "
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="mx-auto py-4 aspect-square w-full object-contain"
+            />
+            <p className="text-gray-600">${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
